451c6dbd7b57b15e27e734d11ee12e707660cffc
Barburon <lamis.htc@gmail.com>
2025-11-21 16:47:18 +0100

    init commit

---
+++ src/main/java/com/xcompany/teamwalk/TeamwalkApplication.java
@@ -0,0 +1,13 @@
+package com.xcompany.teamwalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamwalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamwalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xcompany/teamwalk/TeamwalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcompany.teamwalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamwalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
